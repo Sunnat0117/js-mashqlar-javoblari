7251b8f8bcf34cf3698c92fd07751647145014bc
Sunnat0117 <sunnatqarshiboyev0117@gmail.com>
2021-03-17 21:00:28 +0300

    initial message

---
+++ apps.js
@@ -0,0 +1,104 @@
+
+// examp_11
+aVar = 12/2; // a uzgaruvchiga qiymat hamda amal bajaramiz;
+console.log(aVar); // consolega chiqaramiz
+
+
+// examp_12
+
+var b = 87;
+b = ++b; // bunda qiymat 1 ga oshadi b ++ = b + 1;
+console.log(b);
+
+
+ //examp_13
+
+ let myVar = 14;
+myVar= --myVar;   // javob 13. 
+console.log(myVar);
+
+
+//examp_14
+
+var myDemical = 5.7;
+console.log(myDemical);
+
+
+//examp_15
+
+var product = 2.2 + 1.8;
+console.log(product);
+
+
+//examp_16
+
+
+var  quotient = 4.8/2.0;
+console.log(quotient);
+
+//examp_17
+
+// let  x = Number(prompt(`enter at number`,0));
+// x = x % 4;
+// console.log(x); 
+
+// // examp_18
+
+var a =2, b =3, c = 7;
+ a +=b; // a= a+b a =5
+c+=b;
+
+b+=5; //b =8;
+
+  console.log("a = " +a, "c = "+ c, "b = " + b);
+
+
+
+
+
+//   examp_19
+
+
+
+
+// var a = 11, b =12, c =13 ; 
+// a -= 6; // c=5;
+// b -= 11; // b=1;
+// c-= 9; // c=4;
+// console.log("a = "+ a,"b = " + b,"c = " +c);
+
+
+
+
+
+// // examp_20
+
+// var a = 3, b =4, c =5 ; 
+// a *= 6; // c=5;
+// b *= 11; // b=1;
+// c*= 9; // c=4;
+// console.log("a = "+ a,"b = " + b,"c = " +c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
